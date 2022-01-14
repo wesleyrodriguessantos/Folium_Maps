b581d6c7716057eb002e2c69bcbf5a73b2b314b2
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2022-01-14 15:27:53 -0300

    MarkerCluster - Estudos Iniciados

---
+++ Ex9_Clusters/markerClusterEstados.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+
+ler = pd.read_excel(
+    r"D:\xampp\htdocs\Folium_Maps\Ex9_Clusters\locais.xlsx", engine='openpyxl')
+
+tabela = ler.set_index("UF_2")
+ufs = list(set(tabela.index.values))
+
+
+m = folium.Map(location=[-14.441758, -48.9271955],
+               tiles='OpenStreetMap', zoom_start=5)
+
+cluster = MarkerCluster().add_to(m)
+
+for index, linha in ler.iterrows():
+    folium.Marker([linha["LAT"], linha["LONG"]],
+                  popup='<b>'+linha["LOCALIDADE/UF_2"]+'</b>' + '<br>'+linha["DESCRICAO"], tooltip=linha["LOCALIDADE"],
+                  icon=folium.Icon(
+                      color=linha["COLOR"], icon=linha["ICON"], prefix='fa')).add_to(cluster)
+
+
+cluster.save("Ex9_Clusters/markerclusterEstados.html")
